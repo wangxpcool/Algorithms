059987d612f3e07649ff96a198c0513ec336ddd5
wangxpcool <wangxpcool@qq.com>
2021-05-13 22:09:04 +0800

    Create ConcurrrentHapTest.java

---
+++ src/cn/me/javaBasics/mapInThread/ThreadLocalTest/ConcurrrentHapTest.java
@@ -0,0 +1,32 @@
+package cn.me.javaBasics.mapInThread.ThreadLocalTest;
+
+import java.util.Hashtable;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConcurrrentHapTest {
+
+//    ThreadLocal<ConcurrentHashMap<String, Integer>> map = new ThreadLocal<>();
+    volatile static Integer integer =1;
+    public static void main(String[] args) throws InterruptedException {
+
+
+        ExecutorService executorService = Executors.newFixedThreadPool(20);
+        for (int i = 0; i < 1000; i++) {
+            executorService.execute(new Runnable() {
+                @Override
+                public void run() {
+                    integer++;
+
+                }
+
+            });
+        }
+        Thread.sleep(3000); //模拟等待执行结束
+        System.out.println("------" + integer + "------");
+        executorService.shutdown();
+    }
+
+
+}
